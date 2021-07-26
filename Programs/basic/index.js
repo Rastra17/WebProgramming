c260e0bd975b72021dab4bed016bdfef1e114178
Rastra17 <rastradhoj@gmail.com>
2021-07-26 21:18:21 +0545

    small changes

--- Programs/basic/index.js
+++ Programs/basic/index.js
@@ -1,4 +1,4 @@
-//A program to implement OOP without classes by using HTML file
+//A program to implement constructor without classes
 
 //Constructor function
 function Circle(radius)
@@ -6,9 +6,11 @@ function Circle(radius)
     this.radius=radius;
     this.draw=function()
     {
-        console.log("Draw");
+        let area=(3.14*(radius*radius))
+        console.log("The area of the circle is: "+area);
     }
 }
 
+//In JavaScript, a function is an object
 const circle=new Circle(1);
 circle.draw();
