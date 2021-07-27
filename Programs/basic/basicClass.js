4bc3a2164777d93237316b648ab305e875f37314
Rastra17 <rastradhoj@gmail.com>
2021-07-27 10:35:18 +0545

    new constructor file and some changes

---
+++ Programs/basic/basicClass.js
@@ -0,0 +1,19 @@
+//A program to practice parameterized constructor
+
+class Calc
+{
+    constructor(num1,num2)
+    {
+        this.num1=num1;
+        this.num2=num2;
+
+        this.sum=function()
+        {
+            let add=this.num1+this.num2;
+            console.log("The sum of numbers is: "+add);
+        };
+    }
+}
+
+const display=new Calc(50,50);
+display.sum();
---
+++ Programs/basic/const.js
@@ -0,0 +1,20 @@
+//A program to implement constructor without classes
+
+//Constructor function
+class Circle 
+{
+    constructor(radius) 
+    {
+        this.radius = radius;
+        this.draw = function () 
+        {
+            const pi=3.14;
+            let area = (pi * (radius * radius));
+            console.log("The area of the circle is: " + area);
+        };
+    }
+}
+
+//In JavaScript, a function is an object
+const circle=new Circle(1);
+circle.draw();
