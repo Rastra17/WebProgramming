980a0daddbe7609cada27e272ead4316cdd9ebd7
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2021-12-25 18:10:00 +0545

    some changes and additions

--- Programs/restaurant-reviews/backend/api/restaurants.route.js
+++ Programs/restaurant-reviews/backend/api/restaurants.route.js
@@ -1,9 +1,10 @@
 import express from "express";
+import RestaurantsCtrl from "./restaurants.controller.js";
 
 //Getting access to the express router
 const router=express.Router();
 
 //One route for demonstration
-router.route("/").get((req, res)=>res.send("Hello World"));
+router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
 
 export default router;
